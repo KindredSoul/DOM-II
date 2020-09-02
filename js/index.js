a27f0e63bcb4f1e5b5440ca493d90812dac90239
KindredSoul <jabriltb2@gmail.com>
2020-09-02 16:05:07 -0500

    MVP...I think

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 // ==================================================== Selected Elements and Styles
-const navLinks = document.querySelectorAll('nav .nav-link')
+
+const navLinks = document.querySelectorAll('.nav-link')
 console.log(navLinks)
+const navContainer = document.querySelector('.nav-container')
 
 const logo = document.querySelector('.logo-heading')
-
 
 const contentSections = document.querySelectorAll('.content-section')
 const content1 = (contentSections[0])
 const content2 = (contentSections[1])
 
-// ========================================== Events, Events Everywhere
-// window.addEventListener('load', (event)=> {
-//     let audioElement = new Audio('http://soundbible.com/grab.php?id=611&type=mp3')
-//     audioElement.play()
-// })
+const destinationKeys = document.querySelector('.content-destination h2')
 
-// Main Navigation Events
+const pirate = document.querySelector('.content-destination img')
+
 const fadeIn = {
     transition: 'all 0.5s ease-in 0s',
     transform: 'translateX(0rem)',
@@ -27,6 +25,14 @@ const fadeOut = {
     color: 'transparent'
 }
 
+// ========================================== Events, Events Everywhere
+// window.addEventListener('load', (event)=> {
+//     let audioElement = new Audio('http://soundbible.com/grab.php?id=611&type=mp3')
+//     audioElement.play()
+// })
+
+// Main Navigation Events
+
 logo.addEventListener('dblclick', (e) => {
     e.preventDefault()
     if(e.target.style.color !== 'black'){
@@ -37,8 +43,8 @@ logo.addEventListener('dblclick', (e) => {
 })
 
 navLinks.forEach(item => {
+    item.addEventListener('click', e=> e.preventDefault())
     item.addEventListener('mouseover', (e) => {
-        e.preventDefault()
         e.target.style.color = "orange"
         if (e.target.style.color === "orange"){
             e.target.addEventListener('click', event =>{
@@ -56,12 +62,36 @@ const switcheroo = (content)=>{
 switcheroo(content1)
 switcheroo(content2)
 
+document.body.addEventListener('keydown', e=>{
+    e.preventDefault()
+    destinationKeys.textContent = `Your destination: ${e.code}`
+})
+
+window.addEventListener('scroll', ()=>{
+    let bodyColor =navContainer.style.backgroundColor
+    navContainer.style.transition = ('0.2s ease-in-out')
+    if (bodyColor !== 'blue' && bodyColor !== 'red' && bodyColor !== 'white'){
+        navContainer.style.backgroundColor = 'blue'
+    } else if (bodyColor === 'blue' ){
+        navContainer.style.backgroundColor = 'red'
+    } else if (bodyColor === 'red') {
+        navContainer.style.backgroundColor = 'white'
+    } else if (bodyColor === 'white') {
+        navContainer.style.backgroundColor = 'black'
+    }
+})
 
 
-
-
-
-
+document.addEventListener("visibilitychange", ()=> {
+    let backgroundMusic = new Audio('http://soundbible.com/grab.php?id=1297&type=mp3')
+    if (document.visibilityState === 'hidden') {
+        console.log(document.visibilityState)
+        backgroundMusic.play();
+        backgroundMusic.addEventListener('play', () =>{
+            return alert('Fun Bus is returning from an adventure!')
+        })
+    } 
+});
 
 
 
